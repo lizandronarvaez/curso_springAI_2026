775d3cbb6e44e34abd1b58526e3746d7fe876b5c
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2026-01-09 22:54:39 +0100

    dto de entrada con la pregunta

---
+++ src/main/java/com/lizandronarvaez/spring_ai/dto/GetCapitalRequest.java
@@ -0,0 +1,9 @@
+package com.lizandronarvaez.spring_ai.dto;
+
+/**
+ * Clase: GetCapitalRequest.java
+ * Autor: narvaez
+ * Fecha: 9/1/26
+ */
+public record GetCapitalRequest(String stateOrCountry) {
+}
